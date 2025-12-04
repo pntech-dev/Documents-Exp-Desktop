d1b9805581adfce8a458ff5771715c68936d613c
PN Tech <pavlopavlo2005500@gmail.com>
2025-12-04 08:38:22 +0300

    feat(ui): implement page switching functionality

--- modules/auth/mvc/auth_controller.py
+++ modules/auth/mvc/auth_controller.py
@@ -9,7 +9,39 @@ class AuthController:
 
         """=== Handlers ==="""
         self.view.theme_switcher_clicked(self.on_theme_switcher_clicked)
+        self.view.create_account_button_clicked(self.on_create_account_button_clicked)
+        self.view.change_password_button_clicked(self.on_change_password_button_clicked)
+        self.view.have_account_create_button_clicked(self.on_have_account_create_button_clicked)
+        self.view.have_account_password_button_clicked(self.on_have_account_password_button_clicked)
 
+    """=== Handlers ==="""
 
     def on_theme_switcher_clicked(self) -> None:
         self.view.set_theme()
+
+    # Login page
+
+    def on_create_account_button_clicked(self) -> None:
+        page = self.view.pages.get("signup_page", None)
+        if not page is None:
+            self.view.switch_page(page=page)
+
+    
+    def on_change_password_button_clicked(self, event) -> None:
+        page = self.view.pages.get("change_password_confirm_page", None)
+        if not page is None:
+            self.view.switch_page(page=page)
+
+    # Create account page
+
+    def on_have_account_create_button_clicked(self) -> None:
+        page = self.view.pages.get("login_page", None)
+        if not page is None:
+            self.view.switch_page(page=page)
+
+    # Change password page
+
+    def on_have_account_password_button_clicked(self) -> None:
+        page = self.view.pages.get("login_page", None)
+        if not page is None:
+            self.view.switch_page(page=page)
--- modules/auth/mvc/auth_view.py
+++ modules/auth/mvc/auth_view.py
@@ -8,10 +8,47 @@ class AuthView:
         self.ui = ui
         self.theme_manager = ThemeManager()
 
+        # Auth module pages
+        self.pages = {
+            "login_page": self.ui.login_page,
+            "signup_page": self.ui.signup_page,
+            "change_password_confirm_page": self.ui.change_password_confirm_page,
+            "change_password_change_page": self.ui.change_password_confirm_page
+        }
+
+
     def set_theme(self) -> None:
         """Set theme"""
         self.theme_manager.switch_theme()
-    
+
+
+    def switch_page(self, page) -> None:
+        """Page switcher"""
+        self.ui.pages.setCurrentWidget(page)
+
+    """=== Handlers ==="""
+
     def theme_switcher_clicked(self, handler) -> None:
         """Switch theme on click theme switcher button"""
         self.ui.theme_button.clicked.connect(handler)
+
+    # Login page
+
+    def create_account_button_clicked(self, handler) -> None:
+        self.ui.no_account_pushButton.clicked.connect(handler)
+
+    
+    def change_password_button_clicked(self, handler) -> None:
+        self.ui.fogot_password_label.mousePressEvent = handler
+
+    # Create account page
+
+    def have_account_create_button_clicked(self, handler) -> None:
+        self.ui.have_account_pushButton.clicked.connect(handler)
+
+    # Change password page
+
+    def have_account_password_button_clicked(self, handler) -> None:
+        self.ui.have_account_pushButton_2.clicked.connect(handler)
+
+
